b89e6ba912d3e5c5366f5cc0c781838ad8e8c020
Enis Ozgit <eozgit@gmail.com>
2022-02-05 12:30:00 +0000

    move change list to field in game struct
    colour active cells
    refactor func & field names

--- game.go
+++ game.go
@@ -20,7 +20,8 @@ type Game struct {
 	tiles               map[int]map[int]Tile
 	speed               int
 	shouldIterateCached func(speed int, tick int) bool
-	help                bool
+	showHelp            bool
+	lastUpdates         map[int]map[int]struct{}
 }
 
 func (g *Game) Update() error {
@@ -35,18 +36,30 @@ func (g *Game) Update() error {
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	if g.help {
+	if g.showHelp {
 		ebitenutil.DebugPrint(screen, "0-9 set speed\nh   resume")
 	} else {
 		screen.Fill(color.White)
-		g.sweep(func(i int, j int) {
+		g.scan(func(i int, j int) {
 			if g.tiles[i][j].alive {
 				screen.Set(i, j, color.Black)
 			}
 		})
 
-		if g.tick < 150 {
-			g.sweep(func(i int, j int) {
+		red, aqua := color.RGBA{255, 0, 0, 255}, color.RGBA{0, 255, 255, 255}
+
+		g.scanChanges(func(i int, j int) {
+			var colour color.RGBA
+			if g.tiles[i][j].alive {
+				colour = red
+			} else {
+				colour = aqua
+			}
+			screen.Set(i, j, colour)
+		})
+
+		if g.tick < 80 {
+			g.scan(func(i int, j int) {
 				if j < 19 {
 					screen.Set(i, j, color.Black)
 				}
@@ -63,12 +76,19 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 
 func (g *Game) resetTiles(density float32) {
 	g.tiles = make(map[int]map[int]Tile)
-	g.sweep(func(i int, j int) {
+	g.scan(func(i int, j int) {
 		if g.tiles[i] == nil {
 			g.tiles[i] = make(map[int]Tile)
 		}
 		alive := rand.Float32() < density
 		g.tiles[i][j] = Tile{alive, 0}
+	})
+	g.lastUpdates = make(map[int]map[int]struct{})
+	g.scan(func(i int, j int) {
+		if g.lastUpdates[i] == nil {
+			g.lastUpdates[i] = make(map[int]struct{})
+		}
+		g.lastUpdates[i][j] = struct{}{}
 	})
 }
 
@@ -86,49 +106,27 @@ func (g *Game) checkInput() {
 	}
 
 	if inpututil.IsKeyJustPressed(ebiten.KeyH) {
-		if g.help {
+		if g.showHelp {
 			g.speed = defaultSpeed
 		} else {
 			g.speed = 0
 		}
-		g.help = !g.help
+		g.showHelp = !g.showHelp
 	}
 }
 
 func (g *Game) iterate() {
-	updatedLastRound := make(map[int]map[int]struct{})
-
-	g.sweep(func(i int, j int) {
-		tile := g.tiles[i][j]
-		if tile.iteration == g.iteration-1 {
-			for m := 0; m < 3; m++ {
-				im := i + m - 1
-				if !(im < 0) && im < len(g.tiles) {
-					for n := 0; n < 3; n++ {
-						jn := j + n - 1
-						if !(jn < 0) && jn < len(g.tiles[i]) {
-							if _, ok := updatedLastRound[im]; !ok {
-								updatedLastRound[im] = make(map[int]struct{})
-							}
-							updatedLastRound[im][jn] = struct{}{}
-						}
-					}
-				}
-			}
-		}
-	})
-
-	updates := make(map[int]map[int]bool)
-	for i := range updatedLastRound {
-		for j := range updatedLastRound[i] {
+	updates := make(map[int]map[int]struct{})
+	for i := range g.lastUpdates {
+		for j := range g.lastUpdates[i] {
 			alive := 0
 		neighbourhood:
 			for m := 0; m < 3; m++ {
 				im := i + m - 1
-				if !(im < 0) && im < len(g.tiles) {
+				if !(im < 0) && im < width {
 					for n := 0; n < 3; n++ {
 						jn := j + n - 1
-						if !(jn < 0) && jn < len(g.tiles[i]) {
+						if !(jn < 0) && jn < height {
 							if g.tiles[im][jn].alive {
 								alive++
 							}
@@ -142,30 +140,40 @@ func (g *Game) iterate() {
 
 			if alive == 3 && !g.tiles[i][j].alive {
 				if _, ok := updates[i]; !ok {
-					updates[i] = make(map[int]bool)
+					updates[i] = make(map[int]struct{})
 				}
-				updates[i][j] = true
+				updates[i][j] = struct{}{}
 			}
 
 			if (alive < 3 || alive > 4) && g.tiles[i][j].alive {
 				if _, ok := updates[i]; !ok {
-					updates[i] = make(map[int]bool)
+					updates[i] = make(map[int]struct{})
 				}
-				updates[i][j] = false
+				updates[i][j] = struct{}{}
 			}
 		}
 	}
 
 	for i := range updates {
 		for j := range updates[i] {
-			g.tiles[i][j] = Tile{updates[i][j], g.iteration}
+			g.tiles[i][j] = Tile{!g.tiles[i][j].alive, g.iteration}
+		}
+	}
+
+	g.lastUpdates = updates
+}
+
+func (g *Game) scan(callback func(i int, j int)) {
+	for i := 0; i < width; i++ {
+		for j := 0; j < height; j++ {
+			callback(i, j)
 		}
 	}
 }
 
-func (g *Game) sweep(callback func(i int, j int)) {
-	for i := 0; i < width; i++ {
-		for j := 0; j < height; j++ {
+func (g *Game) scanChanges(callback func(i int, j int)) {
+	for i := range g.lastUpdates {
+		for j := range g.lastUpdates[i] {
 			callback(i, j)
 		}
 	}
